f68748f5963243b5e7bb2861fd50529af49f8871
Feigelman Evgeny <namlegief@gmaail.com>
2016-12-26 23:09:36 +0200

    new menu working

--- cnc/menu.py
+++ cnc/menu.py
@@ -1,48 +1,96 @@
 import tools
-
+from thread import start_new_thread
 from cnc import network
 from cnc import time
-from tasks import agents_list
+
 
 config = tools.get_bind_settings()
 
+def ShowClients():
+    print 'show'
+    EnterToMenu()
 
-def print_header():
 
+def SendCommand():
+    print 'send'
+    EnterToMenu()
+
+
+def SendFile():
+    print 'sendfile'
+    EnterToMenu()
+
+
+def install():
+    print 'install'
+    EnterToMenu()
+
+
+def remove():
+    print 'remove'
+    EnterToMenu()
+
+
+def ShellAccess():
+    print 'shell'
+    EnterToMenu()
+
+
+def NotFound():
+    print "Option not found!"
+    menu(arg=1)
+
+
+def EnterToMenu():
+    try:
+        input("Hit Enter to Back Menu..")
+    except:
+        menu(arg=1)
+
+
+
+def menu(arg=False):
     clock = time.get_current_time()
     segment = network.find_segment(config['cnc_if_name'])
     gateway = network.get_default_ipv4_gw()
     dns_server = network.get_dns_server()
 
     print ("################################################################################")
-    print ("  Python 102          Name: Alex & Jenia                  Time: {0} ".format (clock))
+    print ("  Python 102          Name: Alex & Jenia                  Time: {0} ".format(clock))
     print ("################################################################################")
-    print (" Segment:{0}\n DNS:{1} \n Gateway:{2}\n " .format (segment, dns_server, gateway))
+    print (" Segment:{0}\n DNS:{1} \n Gateway:{2}\n ".format(segment, dns_server, gateway))
     print ("################################################")
     print ("""              Menu
-              ----
-      """)
+                  ----
+          """)
+    print """
+        ################################################################
+        #       Python 102          Name: The girl has no name:)       #
+        ################################################################
+        1) Show clients
+        2) Send commands
+        3) File Transfer
+        4) Installion
+        5) Remove
+        6) Shell on a client """
 
-
-def display_main_menu():
-
-    menu = {
-        '1': 'Show all clients',
-        '2': 'Send commands to all client',
-        '3': 'Transfer file to clients all clients',
-        '4': 'Install something on all clients',
-        '5': 'Remove something from all clients',
+    userChose = raw_input("Choose Options -> ")
+    choice = {
+        "1": ShowClients,
+        "2": SendCommand,
+        "3": SendFile,
+        "4": install,
+        "5": remove,
+        "6": ShellAccess
     }
-    for key, value in menu.iteritems():
-        print(key, '.', value)
+    choice.get(userChose, NotFound)()
 
 
-def display_agent_choice_menu():
-    agents_list.display()
+if __name__ == '__main__':
 
+    try:
 
-def dislay_menu(menu_level):
-    if menu_level == 'main':
-        display_main_menu()
-    if menu_level == 'ag_list':
-        display_agent_choice_menu()
+        start_new_thread(menu(), ('',))
+
+    except Exception, err:
+        print err
